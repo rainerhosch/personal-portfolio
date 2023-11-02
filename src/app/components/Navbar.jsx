fa6b8b9879e46e1c04e5bf87472eba87508e9b97
rz.oktan <rz.oktan@gmail.com>
2023-11-02 11:01:40 +0700

    update logo

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,6 +5,8 @@ import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
+// 👇️ import the image
+// import myImage from '../../../public/images/';
 const navLinks = [
   {
     title: "About",
@@ -25,12 +27,14 @@ const Navbar = () => {
 
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-4xl text-white font-bold"
-        >
-          OKTAN.
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-1 py-2">
+        <Link href={"/"} className="text-2xl md:text-4xl text-white font-bold">
+        <img
+          src="/images/logo-new-1000x300-white.svg"
+          alt="oktan"
+          width={95}
+          height={24}
+        />
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
