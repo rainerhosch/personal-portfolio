2a1e2860710a2c1e11eba2d06fa99842974ec89f
rz.oktan <rz.oktan@gmail.com>
2023-11-02 11:36:32 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
