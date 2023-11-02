a25f4b046ee0688c56305e5854c0c98673fff260
rz.oktan <rz.oktan@gmail.com>
2023-11-02 16:27:08 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,16 +5,29 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rizky Oktan",
-  description: "This my official personal website. ",
+  title: "Rizky Oktan Official Personal Website",
+  type: "article",
+  description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
+  image: "https://rzoktan.tech/images/logo-new-black.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <meta property="og:type" content="article" />
+        <meta
+          property="og:description"
+          content="💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer"
+        />
+        <meta
+          property="og:image"
+          content="https://rzoktan.tech/images/logo-new-black.png"
+        /> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
