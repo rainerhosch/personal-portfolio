c1169607826864d309251693dda1e2ff1c11356c
rz.oktan <rz.oktan@gmail.com>
2023-11-02 17:06:21 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Oktan Portfolio",
-  // description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
   twitter: {
     card: "summary_large_image",
     title: "Rizky Oktan Portfolio",
@@ -17,30 +16,12 @@ export const metadata = {
     creatorId: "1467726470533754880",
     images: ["https://www.rzoktan.tech/images/logo.png"],
   },
-  // images: ['https://rzoktan.tech/images/logo-new-black.png'],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        {/* <meta property="og:type" content="article" />
-        <meta
-          property="og:description"
-          content="💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer"
-        /> */}
-        {/* <meta
-          property="og:image"
-          content="https://rzoktan.tech/images/logo-new-black.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" /> */}
-        {/* <meta property="og:type" content="article" />
-        <meta
-          property="og:image"
-          content="https://flappyowl.vercel.app/newPict400x400-2.png"
-          // content="https://rzoktan.tech/images/logo-new-black.png"
-        /> */}
-        <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
