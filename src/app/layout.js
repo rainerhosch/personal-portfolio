525be56336fc82885e17f7e19cbd9dfafbffdb67
rz.oktan <rz.oktan@gmail.com>
2023-11-02 16:39:16 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,19 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rizky Oktan Official Personal Website",
-  type: "article",
-  description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
-  // image: "https://rzoktan.tech/images/logo-new-black.png",
+  // title: "Rizky Oktan Official Personal Website",
+  // type: "article",
+  // description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
+  // images: ['https://rzoktan.tech/images/logo-new-black.png'],
+  twitter: {
+    card: "summary_large_image",
+    title: "Rizky Oktan Official Personal Website",
+    description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
+    // siteId: '1467726470533754880',
+    creator: "@rzoktan",
+    creatorId: "456456234532364",
+    images: ["https://rzoktan.tech/images/logo-new-black.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -20,11 +29,11 @@ export default function RootLayout({ children }) {
           property="og:description"
           content="💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer"
         /> */}
-        <meta
+        {/* <meta
           property="og:image"
           content="https://rzoktan.tech/images/logo-new-black.png"
         />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
