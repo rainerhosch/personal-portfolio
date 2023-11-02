691aa42c84aece1b68885d51abf5aac5c009c3ad
rz.oktan <rz.oktan@gmail.com>
2023-11-02 16:43:51 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,19 +5,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  // title: "Rizky Oktan Official Personal Website",
-  // type: "article",
-  // description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
+  title: "Rizky Oktan Official Personal Website",
+  description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
   // images: ['https://rzoktan.tech/images/logo-new-black.png'],
-  twitter: {
-    card: "summary_large_image",
-    title: "Rizky Oktan Official Personal Website",
-    description: "💬 Instagram: @kaleng.konguann | 🦜Twitter: @onchainmfer",
-    // siteId: '1467726470533754880',
-    creator: "@rzoktan",
-    creatorId: "456456234532364",
-    images: ["https://rzoktan.tech/images/logo-new-black.png"],
-  },
 };
 
 export default function RootLayout({ children }) {
@@ -34,6 +24,12 @@ export default function RootLayout({ children }) {
           content="https://rzoktan.tech/images/logo-new-black.png"
         />
         <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:image"
+          content="https://rzoktan.tech/images/logo-new-black.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
